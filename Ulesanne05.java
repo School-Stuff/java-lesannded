9ab9472c699938b2f0c9bcfe3929462484d2c3c0
KarlJug <98745340+KarlJug@users.noreply.github.com>
2022-09-07 09:55:14 +0300

    Add files via upload

---
+++ Ulesanne05.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ulesanne05 {
+    // KARL JUGAPUU
+    public static void main(String[] args) {
+
+        int arv1, arv2;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Sisesta arv1: ");
+        arv1 = scanner.nextInt();
+
+        System.out.print("Sisesta arv2: ");
+        arv2 = scanner.nextInt();
+
+        for (int i = 0; i < 3; i++) {
+            arvuKontroll(arv1, arv2);
+        }
+
+    }
+
+
+    static void arvuKontroll (int arv1, int arv2) {
+
+        if (arv1 >= 0 && arv2 > 0) {
+            System.out.println("Midagi on valesti.");
+        }
+
+        double vastus = arv1 / arv2;
+
+        System.out.println(vastus);
+    }
+}
